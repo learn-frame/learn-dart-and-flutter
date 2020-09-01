4a2b5c1526d4c3781cedda34e680cd0001bbc17d
Yancey Leo <yanceyofficial@gmail.com>
2020-09-01 09:20:23 +0800

    feat: learn numbers

--- notes/Built-in types/numbers.dart
+++ notes/Built-in types/numbers.dart
@@ -4,6 +4,9 @@
 /// 成 JavaScript 就是 -2^53 至 2^53 - 1.
 ///
 /// double 为 64 位的双精度浮点数字, 且符合 IEEE 754 标准
+///
+/// 对于一些数学运算, 你可以查看 dart:math 库中的 API
+/// https://api.dart.dev/stable/dart-math/dart-math-library.html
 
 void main() {
   // int 的属性和方法可参考
@@ -13,6 +16,9 @@ void main() {
   intNum.toString();
   intNum.isEven;
 
+  // 也可以定义其他进制的整数
+  int hex = 0xDEADBEEF;
+
   // double 的属性和方法可参考
   // https://api.dart.dev/stable/dart-core/double-class.html
   final doubleNum = 100.1;
@@ -20,8 +26,17 @@ void main() {
   doubleNum.toString();
   doubleNum.isInfinite;
 
-  /// TODO: 除了加减乘除和一些内置属性方法, 它们还可以使用一些位运算
-  ///
-  /// 对于一些数学运算, 你还可以查看 dart:math 库中的 API
-  /// https://api.dart.dev/stable/dart-math/dart-math-library.html
+  // 将 int 类型的数字定义成 double 类型, 该变量会被转换成 double 类型
+  double z = 1; // z 变成了 1.0
+
+  // 字符串和数字类型之间的转换
+  int.parse('1');
+  double.parse('1.1');
+  1.toString();
+  3.14159.toStringAsFixed(2);
+
+  // int 类型的数字可以使用一些位运算符
+  assert((3 << 1) == 6); // 0011 << 1 == 0110
+  assert((3 >> 1) == 1); // 0011 >> 1 == 0001
+  assert((3 | 4) == 7); // 0011 | 0100 == 0111
 }
