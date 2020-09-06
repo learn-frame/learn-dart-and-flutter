cb9b5187764d01fe0e1459c2afd1eafbef34c009
Yancey Leo <yanceyofficial@gmail.com>
2020-09-06 14:23:38 +0800

    feat: learn booleans

--- docs/learn-dart/Built-in types/booleans.dart
+++ docs/learn-dart/Built-in types/booleans.dart
@@ -1,3 +1,19 @@
 /// Boolean 类型
 
-void main() {}
+void main() {
+  // 检查是否为空字符串
+  var fullName = '';
+  print(fullName.isEmpty); // true
+
+  // 检查是否小于等于零
+  var hitPoints = 0;
+  print(hitPoints <= 0); // true
+
+  // 检查是否为 null
+  var unicorn;
+  print(unicorn == null); // true
+
+  // 检查是否为 NaN
+  var iMeantToDoThis = 0 / 0;
+  print(iMeantToDoThis.isNaN); // true
+}
