c4db072e20e399e897367218b7de89a978c0db35
Yancey Leo <yanceyofficial@gmail.com>
2020-09-08 20:32:35 +0800

    feat: learn map

--- docs/learn-dart/Built-in types/map.dart
+++ docs/learn-dart/Built-in types/map.dart
@@ -17,4 +17,18 @@ void main() {
   nobleGases[2] = 'helium';
   nobleGases[10] = 'neon';
   nobleGases[18] = 'argon';
+
+  gifts['one'] = 'hello'; // 向 map 中添加一个键值对
+  gifts['one']; // 从 map 获取一个值, 当取不到时, 返回 null
+  gifts.length; // 获取 map 的长度
+
+  // 可通过在 Map 字面量前添加 const 关键字创建一个 Map 编译时常量
+  // 此时的 constantMap 将不能使用 add / addAll 等方法
+  final constantMap = const {
+    'first': 'partridge',
+    'second': 'turtledoves',
+  };
+
+  // Unsupported operation: Cannot change unmodifiable map
+  // constantMap['first'] = 'money';
 }
