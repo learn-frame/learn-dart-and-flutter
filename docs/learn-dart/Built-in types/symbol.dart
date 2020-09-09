b6eae9684c0e3305d8fb80921e819dbf7b33a6c6
Yancey Leo <yanceyofficial@gmail.com>
2020-09-09 14:54:52 +0800

    feat: learn symbol

--- docs/learn-dart/Built-in types/symbol.dart
+++ docs/learn-dart/Built-in types/symbol.dart
@@ -1,2 +1,14 @@
 /// Symbol
 ///
+/// A Symbol object represents an operator or identifier declared in a
+/// Dart program. You might never need to use symbols, but they’re invaluable
+/// for APIs that refer to identifiers by name, because minification changes
+/// identifier names but not identifier symbols.
+
+void main() {
+  #foo;
+  #bar;
+
+  Symbol object = new Symbol('name');
+  print(object); // Symbol("name")
+}
