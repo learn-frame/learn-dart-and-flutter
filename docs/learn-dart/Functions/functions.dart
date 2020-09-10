16488489140800e12a324a06831d3ebfc5e0c639
Yancey Leo <yanceyofficial@gmail.com>
2020-09-10 23:32:57 +0800

    feat: learn fucntion

--- docs/learn-dart/Functions/functions.dart
+++ docs/learn-dart/Functions/functions.dart
@@ -1,1 +1,13 @@
 /// 函数
+///
+
+const _nobleGases = [1, 2, 3];
+
+bool isNoble(int atomicNumber) {
+  return _nobleGases[atomicNumber] != null;
+}
+
+void main() {
+  print(isNoble(1)); // true
+  // print(isNoble(3)); // 数组越界
+}
