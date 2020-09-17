285b95edcde2848440119915a6271494b3715c1d
Yancey Leo <yanceyofficial@gmail.com>
2020-09-17 09:39:22 +0800

    feat: learn constructor

--- documents/learn-dart/Classes/constructor.dart
+++ documents/learn-dart/Classes/constructor.dart
@@ -5,26 +5,28 @@
 ///
 /// 在 Dart 2.x 后可以不用使用 new 来创建实例
 ///
-/// TODO: 常量构造函数, 常量上下文 
+/// TODO: 常量构造函数, 常量上下文
 
 class Person {
   String firstName;
   String lastName;
   int age;
 
-  // 和其他语言一样声明构造函数的语法
+  // 无参数的构造函数:
+  // 如果没有声明构造函数, 那么 Dart 会自动生成一个无参数的构造函数
+  // 并且该构造函数会调用其父类的无参数构造方法
+
+  // 声明构造函数
   // Person(String firstName, String lastName, int age) {
   //   this.firstName = firstName;
   //   this.lastName = lastName;
   //   this.age = age;
   // }
 
-  // Dart 独有的语法糖
+  // Dart 独有的语法糖来声明构造函数
   Person(this.firstName, this.lastName, this.age);
 
   // 命名式构造函数
-  // TODO: 子类不能继承父类的命名式构造函数, 如果你想在子类中提供一个与父类命名
-  // 构造函数名字一样的命名构造函数, 则需要在子类中显式地声明
   Person.defaultPerson(String firstName) {
     firstName = firstName;
     lastName = 'Leo';
@@ -32,11 +34,26 @@ class Person {
   }
 }
 
+// 子类调用父类非默认构造函数的顺序:
+// 1.初始化列表, 2.父类的无参数构造函数, 3.当前类的无参数构造函数
 class Male extends Person {
   int incoming;
 
+  // 继承
+  // Male(String firstName, String lastName, int age, this.incoming)
+  //     : super(firstName, lastName, age);
+
+  // 如果继承命名构造函数, 可以这样写
   Male(String firstName, String lastName, int age, this.incoming)
-      : super(firstName, lastName, age);
+      : super.defaultPerson(firstName);
+
+  // 子类的构造函数调用了父类的命名构造函数
+  // 子类不能继承父类的命名式构造函数, 如果你想在子类中提供一个与父类命名
+  // 构造函数名字一样的命名构造函数, 则需要在子类中显式地声明
+  // Male.defaultPerson(String firstName) : super.defaultPerson(firstName) {
+  //   // do something...
+  // }
+
 }
 
 void main(List<String> args) {
