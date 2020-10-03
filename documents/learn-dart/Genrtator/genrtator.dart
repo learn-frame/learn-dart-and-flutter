379bc0e70a1928949d13009e39f70d3891067e5f
Yancey Leo <yanceyofficial@gmail.com>
2020-10-03 23:54:33 +0800

    feat: learn generator

--- documents/learn-dart/Genrtator/genrtator.dart
+++ documents/learn-dart/Genrtator/genrtator.dart
@@ -7,6 +7,18 @@ Iterable<int> naturalsTo(int n) sync* {
 }
 
 // 异步生成器
+Stream<int> asynchronousNaturalsTo(int n) async* {
+  int k = 0;
+  while (k < n) yield k++;
+}
+
+// 生成器的递归调用, 可使用 yield* 语句提升执行性能
+Iterable<int> naturalsDownFrom(int n) sync* {
+  if (n > 0) {
+    yield n;
+    yield* naturalsDownFrom(n - 1);
+  }
+}
 
 void main(List<String> args) {
   print(naturalsTo(5)); // (0, 1, 2, 3, 4)
