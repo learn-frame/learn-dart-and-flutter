2c430e444b87ae21a0a147d855ec781a104b9715
Yancey Leo <yanceyofficial@gmail.com>
2020-10-12 22:57:04 +0800

    feat: learn generics

--- documents/learn-dart/Generics/generics.dart
+++ documents/learn-dart/Generics/generics.dart
@@ -3,3 +3,9 @@
 /// 泛型常用于需要要求类型安全的情况, 但是它也会对代码运行有好处:
 /// 1. 适当地指定泛型可以更好地帮助代码生成
 /// 2. 使用泛型可以减少代码重复
+
+void main() {
+  final names = List<String>();
+  names.addAll(['Yancey', 'Sayaka']);
+  // names.add(1); // 出错, names 只能接受字符串类型的参数
+}
