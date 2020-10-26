03b0668ac7fcf35bed76e628eaf1de9af6242b51
Yancey Leo <yanceyofficial@gmail.com>
2020-10-26 08:42:59 +0800

    feat: learn implicit_interfaces

--- documents/learn-dart/Classes/implicit_interfaces.dart
+++ documents/learn-dart/Classes/implicit_interfaces.dart
@@ -1,3 +1,23 @@
-/// 隐式接口
+/// 隐式接口, 其实就是 implements
 ///
 ///
+
+class Person {
+  final _name;
+
+  Person(this._name);
+
+  String greet(String who) => '你好，$who。我是$_name。';
+}
+
+class Impostor implements Person {
+  @override
+  // TODO: implement _name
+  get _name => throw UnimplementedError();
+
+  @override
+  String greet(String who) {
+    // TODO: implement greet
+    throw UnimplementedError();
+  }
+}
