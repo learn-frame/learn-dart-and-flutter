2ed5518fe96d98cef8b6905be76d5736d6aaca60
Yancey Leo <yanceyofficial@gmail.com>
2020-11-03 14:04:33 +0800

    feat: update code structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+// Copyright (c) 2020 Yancey Inc. and its affiliates.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
-import './pages/home.dart';
-import './widgets/bottom_navigation_bar.dart';
+import 'screens/home.dart';
+import 'widgets/bottom_navigation_bar.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(YanceyBlog());
 
-/// The main entry for this app.x
-class MyApp extends StatelessWidget {
+/// The main entry for this app.
+class YanceyBlog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
